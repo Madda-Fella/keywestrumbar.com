db7293629cc2c6204c3939ab614754922a8ee5cf
Asa Smith <smith.asa.la@gmail.com>
2018-10-22 18:01:01 -0400

    add photo gallery lightbox

--- assets/js/index.js
+++ assets/js/index.js
@@ -70,3 +70,75 @@ setTimeout(function() {if (location.hash) {
   scrollSite(location.hash);
   window.location.hash = '';
 }}, 10);
+
+// lightbox-gallery
+  if (document.querySelector('#lightbox-gallery')) {
+    const body = document.querySelector('body')
+    const galleryParent = document.querySelector('.mf-gallery')
+    const galleryImgArr = Array.from(document.querySelectorAll('.lightbox-img'))
+    const lightboxGallery = document.querySelector('#lightbox-gallery')
+    const lightboxImg = document.querySelector('.lightbox-gallery__img')
+    const imagePathArr = galleryImgArr.map(image => image.dataset.img)
+    const nextBtn = document.querySelector('.next')
+    const prevBtn = document.querySelector('.prev')
+    const closeBtn = document.querySelector('.lightbox__close')
+
+    const setImage = (e) => {
+      console.log(e.target)
+      lightboxImg.setAttribute('src', e.target.dataset.img)
+      lightboxImg.setAttribute('data-img', e.target.dataset.img)
+    }
+
+    const nextImg = () => {
+      const currentImg = document.querySelector('.lightbox-gallery__img')
+      let currentImgPath = currentImg.dataset.img
+      let currentIndex = imagePathArr.indexOf(currentImgPath)
+      let nextImgPath = currentIndex === 6 ? 0 : currentIndex + 1
+      console.log(currentIndex)
+      console.log(imagePathArr[currentIndex + 1])
+
+      currentImg.setAttribute('src', imagePathArr[nextImgPath])
+      currentImg.setAttribute('data-img', imagePathArr[nextImgPath])
+    }
+
+    const prevImg = () => {
+      const currentImg = document.querySelector('.lightbox-gallery__img')
+      let currentImgPath = currentImg.dataset.img
+      let currentIndex = imagePathArr.indexOf(currentImgPath)
+      let prevImgPath = currentIndex === 0 ? 6 : currentIndex - 1
+      console.log(currentIndex)
+      console.log(imagePathArr[currentIndex - 1])
+
+      currentImg.setAttribute('src', imagePathArr[prevImgPath])
+      currentImg.setAttribute('data-img', imagePathArr[prevImgPath])
+    }
+
+    closeBtn.addEventListener('click', () => {
+      lightboxGallery.classList.add('is-hidden')
+      lightboxGallery.classList.remove('is-shown')
+      body.classList.remove('modal-shown')
+    })
+
+    nextBtn.addEventListener('click', () => {
+      nextImg()
+    })
+
+    prevBtn.addEventListener('click', () => {
+      prevImg()
+    })
+
+    galleryParent.addEventListener('click', (e) => {
+
+      setImage(e)
+
+      if (lightboxGallery.classList.contains('is-hidden')) {
+        lightboxGallery.classList.add('is-shown')
+        lightboxGallery.classList.remove('is-hidden')
+        body.classList.add('modal-shown')
+      } else {
+        lightboxGallery.classList.add('is-hidden')
+        lightboxGallery.classList.remove('is-shown')
+        body.classList.remove('modal-shown')
+      }
+    })
+  }
